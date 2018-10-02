b47537dc492fbb713e050541ace098004f3cffe7
CbAqvq191 <37643006+CbAqvq191@users.noreply.github.com>
2018-10-01 22:41:00 -0300

    Initial commit

---
+++ app/src/main/java/com/youtube/aquevequesebastianandroidtrabajopractico1/MainActivity_inicio.kt
@@ -0,0 +1,12 @@
+package com.youtube.aquevequesebastianandroidtrabajopractico1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_inicio : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_inicio)
+    }
+}
